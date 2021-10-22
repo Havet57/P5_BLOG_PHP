8bb0c4a55d30fee8470724225fa9ceb3e7699390
Havet-68 <64773554+Havet-68@users.noreply.github.com>
2021-10-22 10:39:29 +0200

    Ajout formulaire contact

--- accueil.php
+++ accueil.php
@@ -14,6 +14,16 @@
         <h1>RABEHI Sofia</h1>
         <h2>Développeuse en perpétuelle devenir</h2>
 
+        <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+
+
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/umd/popper.min.js"></script>
