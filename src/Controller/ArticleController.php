30ed11792919eb7003f57b43e1e00f552c2566fe
Havet-68 <64773554+Havet-68@users.noreply.github.com>
2021-12-10 16:22:43 +0100

    Améliorations user, articles, homepage.
    Amélioration login register,  articles, admin user début distinction. Reglage chemin images sur homepage et page tous les articles.

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,56 @@
+<?php 
+
+namespace App\Controller;
+use App\Repository\ArticleRepository;
+
+class ArticleController extends CoreController {
+    public function displayArticle(int $id){      
+        $repository = new ArticleRepository;
+        $article=$repository->find($id);
+        echo $this->twig->render('article.html.twig', ['article' => $article]);
+        
+    }
+
+    public function allArticles(){
+        if(!empty($this->user)){
+            $articleRepository = new ArticleRepository;
+            $articles = $articleRepository->findAll();
+            echo $this->twig->render('touslesarticles.html.twig', ['articles' => $articles, 'user'=> $this->user]);
+        }else{
+            header('location: index.php?controller=authentification&methode=login');
+        }
+    }
+
+    public function createArticle(){
+        if ((!empty($_POST['textTitle'])) && (!empty($_POST['textContent']))) {
+            $ArticleRepository = new ArticleRepository;
+            $ArticleRepository->save($_POST['textTitle'], $_POST['textContent']);  
+        }
+        echo $this->twig->render('ecrireunarticle.html.twig');
+    }
+
+    public function updateArticle(int $id){
+        if($this->user['is_admin']){
+            $articleRepository = new ArticleRepository;
+
+            if(!empty($_POST['textTitle'])){
+                $articleRepository->update($_POST['textTitle'], $_POST['textContent'], $id);
+                header('Location: index.php');
+            }
+            $article = $articleRepository->find($id);
+
+            echo $this->twig->render('modifiearticle.html.twig', ['article' => $article ]);
+        }else{
+            echo 'bien tenté';
+        }
+    }
+
+    public function deleteArticle(int $id){
+        $articleRepository = new ArticleRepository;
+        $articleRepository->delete($id);
+
+        header('location: index.php?controller=article&methode=tous');
+
+        
+    }
+}
--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -7,6 +7,6 @@ class HomePageController extends CoreController {
     public function home (){      
         $repository = new ArticleRepository;
         $articles=$repository->findMostRecent();
-        echo $this->twig->render('homepage.html.twig', ['articles' => $articles]);
+        echo $this->twig->render('homepage.html.twig', ['articles' => $articles, 'user' => $this->user]);
     }
 }
