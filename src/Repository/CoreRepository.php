8da5f851538dde7d761f2140dd8ba0a95d93cef3
Havet-68 <64773554+Havet-68@users.noreply.github.com>
2022-02-03 13:24:46 +0100

    style finitions

--- src/Repository/CoreRepository.php
+++ src/Repository/CoreRepository.php
@@ -3,16 +3,14 @@
 namespace App\Repository;
 
 class CoreRepository {
-    const DB_USERNAME='root';
-    const DB_PASSWORD='';
-    const DB_NAME="blog";
-    const DB_SERVER='localhost';
+    
 
     protected \PDO $pdo;
 
     public function __construct()
     {
-        $this->pdo=new \PDO('mysql:host='.self::DB_SERVER.';dbname='.self::DB_NAME.';charset=utf8', self::DB_USERNAME, self::DB_PASSWORD);
+        $config=json_decode(file_get_contents('config/database.json'));
+        $this->pdo=new \PDO('mysql:host='.$config->DB_SERVER.';dbname='.$config->DB_NAME.';charset=utf8', $config->DB_USERNAME, $config->DB_PASSWORD);
         $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
     }
 }
